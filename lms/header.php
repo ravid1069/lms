4ee8f7a394d331640999bdc4d978f4dcf1745115
ravid1069 <147243311+ravid1069@users.noreply.github.com>
2024-07-25 04:04:24 +0530

    Add files via upload

---
+++ lms/header.php
@@ -0,0 +1,62 @@
+<header class="header" data-header>
+        <div class="container">
+
+            <a href="#" class="logo">
+                <img src="./assets/images/nit2.png" width="100" height="100" alt="NTS logo">
+            </a>
+
+            <nav class="navbar" data-navbar>
+
+                <div class="wrapper">
+                    <a href="#" class="logo">
+                        <img src="./assets/images/logo.svg" width="162" height="50" alt="EduWeb logo">
+                    </a>
+
+                    <button class="nav-close-btn" aria-label="close menu" data-nav-toggler>
+                        <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+                    </button>
+                </div>
+
+                <ul class="navbar-list">
+
+                    <li class="navbar-item">
+                        <a href="dashboard.php" class="navbar-link" data-nav-link>Dashboard</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="subjects.php" class="navbar-link" data-nav-link>Courses for you</a>
+                    </li>
+
+                    <li class="navbar-item">
+                        <a href="#courses" class="navbar-link" data-nav-link>Coding Events</a>
+                    </li>
+
+                    <li class="navbar-item">
+                        <a href="#blog" class="navbar-link" data-nav-link>Code Files</a>
+                    </li>
+
+                    <li class="navbar-item">
+                        <a href="#" class="navbar-link" data-nav-link>Contact</a>
+                    </li>
+
+                </ul>
+
+            </nav>
+
+            <div class="header-actions">
+
+                <a href="logout.php" class="btn has-before">
+                    <span class="span">Log Out</span>
+
+                    <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                </a>
+
+                <button class="header-action-btn" aria-label="open menu" data-nav-toggler>
+                    <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+                </button>
+
+            </div>
+
+            <div class="overlay" data-nav-toggler data-overlay></div>
+
+        </div>
+    </header>
